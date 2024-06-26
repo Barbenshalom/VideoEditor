bc6dc66fb9564fa99310086ddc88507647e95cda
Barbenshalom <66558610+Barbenshalom@users.noreply.github.com>
2024-06-26 11:45:25 +0300

    Delete src directory

--- src/app/app.component.types.ts
+++
@@ -1,7 +0,0 @@
-export type TrackScene = {
-  src: string;
-  sceneName: string;
-  /** DOM element width */
-  width: number;
-  id: string;
-};
--- src/app/scenes.ts
+++
@@ -1,5 +0,0 @@
-export const SCENES = [
-  'https://content.shuffll.com/files/background-music/1.mp4',
-  'https://content.shuffll.com/files/background-music/2.mp4',
-  'https://content.shuffll.com/files/background-music/3.mp4',
-] as const;
